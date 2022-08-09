473bb03fb892b768b31f28fd3aa530007e72824d
Admin <andrew.wkspace@gmail.com>
2022-08-09 23:02:02 +0300

    commit

--- loader.js
+++ loader.js
@@ -1,9 +1,28 @@
 var myVar;
-
+  
 function myFunction() {
-  myVar = setTimeout(showPage, 4000);
+  myVar = setTimeout(showPage, 5000);
 }
 function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("my_content").style.display = "block";
 }
+function chargebattery() {
+  var a;
+  a = document.getElementById("loader");
+  a.innerHTML = "&#xf244;";
+  setTimeout(function () {
+    a.innerHTML = "&#xf243;";
+  }, 1000);
+  setTimeout(function () {
+    a.innerHTML = "&#xf242;";
+  }, 2000);
+  setTimeout(function () {
+    a.innerHTML = "&#xf241;";
+  }, 3000);
+  setTimeout(function () {
+    a.innerHTML = "&#xf240;";
+  }, 4000);
+}
+chargebattery();
+setInterval(chargebattery, 5000);
